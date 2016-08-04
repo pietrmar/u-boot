93651457f6be0fa686c688a6f5f1c7a8cb9c5560
Martin Pietryka <martin.pietryka@streamunlimited.com>
2016-08-04 18:28:40 +0200

    power: axp152: various extensions to support stream810
    Including:
     * add support for dcdc1
     * add support for ldo0 and ldo1
     * add support for aldo1 and aldo2
     * set the `reverse of power on sequence` bit on axp152_init()
     * enable power recovery
    
    Signed-off-by: Martin Pietryka <martin.pietryka@streamunlimited.com>

--- drivers/power/axp152.c
+++ drivers/power/axp152.c
@@ -7,15 +7,16 @@
 #include <common.h>
 #include <i2c.h>
 #include <axp152.h>
+#include <errno.h>
 
 static int axp152_write(enum axp152_reg reg, u8 val)
 {
-	return i2c_write(0x30, reg, 1, &val, 1);
+	return i2c_write(AXP152_ADDR, reg, 1, &val, 1);
 }
 
 static int axp152_read(enum axp152_reg reg, u8 *val)
 {
-	return i2c_read(0x30, reg, 1, val, 1);
+	return i2c_read(AXP152_ADDR, reg, 1, val, 1);
 }
 
 static u8 axp152_mvolt_to_target(int mvolt, int min, int max, int div)
@@ -26,6 +27,14 @@ static u8 axp152_mvolt_to_target(int mvolt, int min, int max, int div)
 		mvolt = max;
 
 	return (mvolt - min) / div;
+}
+
+int axp152_set_dcdc1(enum axp152_dcdc1_voltages volt)
+{
+	if (volt < AXP152_DCDC1_1V7 || volt > AXP152_DCDC1_3V5)
+		return -EINVAL;
+
+	return axp152_write(AXP152_DCDC1_VOLTAGE, volt);
 }
 
 int axp152_set_dcdc2(int mvolt)
@@ -63,6 +72,20 @@ int axp152_set_dcdc4(int mvolt)
 	return axp152_write(AXP152_DCDC4_VOLTAGE, target);
 }
 
+int axp152_set_ldo0(enum axp152_ldo0_volts volt, enum axp152_ldo0_curr_limit curr_limit)
+{
+	u8 target = curr_limit | (volt << 4) | (1 << 7);
+
+	return axp152_write(AXP152_LDO0_VOLTAGE, target);
+}
+
+int axp152_set_ldo1(int mvolt)
+{
+	u8 target = axp152_mvolt_to_target(mvolt, 700, 3500, 100);
+
+	return axp152_write(AXP152_LDO1_VOLTAGE, target);
+}
+
 int axp152_set_ldo2(int mvolt)
 {
 	u8 target = axp152_mvolt_to_target(mvolt, 700, 3500, 100);
@@ -70,17 +93,65 @@ int axp152_set_ldo2(int mvolt)
 	return axp152_write(AXP152_LDO2_VOLTAGE, target);
 }
 
+int axp152_set_aldo1(enum axp152_aldo_voltages volt)
+{
+	u8 val;
+	int ret;
+
+	ret = axp152_read(AXP152_ALDO1_ALDO2_VOLTAGE, &val);
+	if (ret)
+		return ret;
+
+	val |= (volt << 4);
+	return axp152_write(AXP152_ALDO1_ALDO2_VOLTAGE, val);
+}
+
+int axp152_set_aldo2(enum axp152_aldo_voltages volt)
+{
+	u8 val;
+	int ret;
+
+	ret = axp152_read(AXP152_ALDO1_ALDO2_VOLTAGE, &val);
+	if (ret)
+		return ret;
+
+	val |= volt;
+	return axp152_write(AXP152_ALDO1_ALDO2_VOLTAGE, val);
+}
+
+int axp152_set_power_output(int val)
+{
+	return axp152_write(AXP152_POWER_CONTROL, val);
+}
+
 int axp152_init(void)
 {
-	u8 ver;
-	int rc;
+	u8 reg;
+	int ret;
 
-	rc = axp152_read(AXP152_CHIP_VERSION, &ver);
-	if (rc)
-		return rc;
+	ret = axp152_read(AXP152_CHIP_VERSION, &reg);
+	if (ret)
+		return ret;
 
-	if (ver != 0x05)
+	if (reg != 0x05)
 		return -1;
 
-	return 0;
+
+	/* Set the power off sequence to `reverse of power on sequence` */
+	ret = axp152_read(AXP152_SHUTDOWN, &reg);
+	if (ret)
+		return ret;
+	reg |= AXP152_POWEROFF_SEQ;
+	ret = axp152_write(AXP152_SHUTDOWN, reg);
+	if (ret)
+		return ret;
+
+
+	/* Enable the power recovery */
+	ret = axp152_read(AXP152_POWER_RECOVERY, &reg);
+	if (ret)
+		return ret;
+	reg |= AXP152_POWER_RECOVERY_EN;
+	ret = axp152_write(AXP152_POWER_RECOVERY, reg);
+	return ret;
 }
