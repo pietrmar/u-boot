2411b3483bf0b9c3cbfce145b7b526188905fe79
Martin Pietryka <martin.pietryka@streamunlimited.com>
2016-10-25 19:12:20 +0200

    board: sue: demo-client: implement alternative fwupdate request
    Signed-off-by: Martin Pietryka <martin.pietryka@streamunlimited.com>

--- board/streamunlimited/common/carrier_sue_demo_client.c
+++ board/streamunlimited/common/carrier_sue_demo_client.c
@@ -6,26 +6,46 @@
 
 #define FWUP_GPIO IMX_GPIO_NR(6, 20)
 
+#define ALT_FWUP_TIMEOUT	3000	/* in ms */
+#define ALT_FWUP_GPIO IMX_GPIO_NR(3, 21)
+
+static int alt_fwup_request = 0;
+
 static const iomux_v3_cfg_t fwupdate_pads[] = {
 	MX7D_PAD_SAI2_TX_BCLK__GPIO6_IO20 | MUX_PAD_CTRL(PAD_CTL_PUE | PAD_CTL_PUS_PU5KOHM),
+	MX7D_PAD_LCD_DATA15__GPIO3_IO20 | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
 
 static int demo_client_init(const struct sue_device_info *device)
 {
 	imx_iomux_v3_setup_multiple_pads(fwupdate_pads, ARRAY_SIZE(fwupdate_pads));
 	gpio_direction_input(FWUP_GPIO);
+	gpio_direction_input(ALT_FWUP_GPIO);
 
 	return 0;
 }
 
 static int demo_client_late_init(const struct sue_device_info *device)
 {
+	int totaldelay = 0;
+
+	while (gpio_get_value(ALT_FWUP_GPIO)) {
+		if (totaldelay > ALT_FWUP_TIMEOUT) {
+			printf("Alt fwup request set\n");
+			alt_fwup_request = 1;
+			break;
+		}
+
+		mdelay(100);
+		totaldelay += 100;
+	}
+
 	return 0;
 }
 
 static int demo_client_get_usb_update_request(const struct sue_device_info *device)
 {
-	return gpio_get_value(FWUP_GPIO);
+	return alt_fwup_request || !gpio_get_value(FWUP_GPIO);
 }
 
 struct sue_carrier_ops demo_client_ops = {
